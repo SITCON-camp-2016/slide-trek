3aafd3acff980c37ff71ce50797f05924ee3fe0e
Hao-Yung Chan <hychan@caece.net>
2016-08-05 12:19:36 +0800

    stage 3 : demo version

--- app.js
+++ app.js
@@ -1,31 +1,67 @@
+var passwd = "WeLoveNTUOSC!"
+
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 
+var cheerio = require('cheerio');
+var fs = require('fs');
+
 server.listen(5000);
 
-var speaker = "";
+app.set("view engine", "ejs");
 
-app.use(express.static(__dirname + "/slides/reveal.js"));
+app.get('/:slide_title/:slide_name',function(req, res){
+  console.log(__dirname);
+  var slide_title = req.params.slide_title;
+  var slide_name = req.params.slide_name;
+  console.log("Slide Folder Name: " + slide_title);
+  console.log("Slide File Name: " + slide_name);
+  var path = __dirname + "/slides/" + slide_title + "/" + slide_name;
+
+  fs.readFile(path, function(err, data){
+    var $ = cheerio.load(data);
+    var slide_head = $('head').html();
+    var slide_body = $('body').html();
+
+    do_render(res, slide_head, slide_body);
+  });
+});
+
+app.use(express.static(__dirname + "/slides"));
+
+function do_render(res, slide_head, slide_body) {
+  res.render(__dirname + "/template/st-view", {
+    slide_head : slide_head,
+    slide_body : slide_body,
+  });
+}
 
 io.on('connection', function(socket){
   /* Speaker Events */
-  socket.on('direction_up', function() {
-    socket.broadcast.emit('direction_up');
-    console.log("up")
+  socket.on('passwd',function(data){
+    if (passwd != data) {
+      return;
+    } else {
+      socket.broadcast.emit('is_audience', true);
+    }
   });
-  socket.on('direction_down', function() {
-    socket.broadcast.emit('direction_down');
-    console.log("down")
+
+  socket.on('pagechange', function(data) {
+    if (passwd != data.passwd) {
+      return;
+    } else {
+      socket.broadcast.emit('pagechange', data.state);
+    }
   });
-  socket.on('direction_left', function() {
-    socket.broadcast.emit('direction_left');
-    console.log("left")
-  });
-  socket.on('direction_right', function() {
-    socket.broadcast.emit('direction_right');
-    console.log("right")
+
+  socket.on('mousemove', function(data) {
+    if (passwd != data.passwd) {
+      return;
+    } else {
+      socket.broadcast.emit('mousemove', data.mousePos);
+    }
   });
 
   socket.on('disconnect', function(){});
